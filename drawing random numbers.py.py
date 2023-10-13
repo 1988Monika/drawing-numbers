5ad38ab260b41e4adcbf1cee44745882a424a4f2
Monika Siekierczak <124308488+1988Monika@users.noreply.github.com>
2023-10-13 14:10:29 +0200

    Add files via upload

---
+++ drawing random numbers.py.py
@@ -0,0 +1,28 @@
+import random
+
+def will_weapon_hit(weaponChanceToHitPercentage):
+    isHitChance = random.uniform(0,100)
+    if (isHitChance < weaponChanceToHitPercentage):
+        return "hit"
+    else:
+        return "not hit"
+
+
+x = 0
+
+listHit = []
+
+while x < 100:
+    x = x + 1
+    listHit.append(will_weapon_hit(50))
+
+from collections import Counter
+
+dictionaryHit = Counter(listHit)
+
+x = 0
+
+
+while x < 100:
+    x = x + 1
+    print(random.randint(0,10))
